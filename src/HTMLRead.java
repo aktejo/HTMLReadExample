f08ecce447a32195b62dea4530d018930bc09634
aktejo <abhiram_tejomurtula25@milton.edu>
2024-02-15 09:04:02 -0500

    First Push

--- src/HTMLRead.java
+++ src/HTMLRead.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -19,37 +23,92 @@ build gui:
 
  */
 
-public class HTMLRead {
+public class HTMLRead implements ActionListener {
 
     public static void main(String[] args) {
         HTMLRead html = new HTMLRead();
     }
+    private JFrame mainFrame;
+    private JPanel newPanel;
+    private JTextField searchBox;
+    private JTextArea searchResults;
+    private JButton goButton;
+    private JTextField website;
+    private JScrollPane scrollPane;
+
+    String searchTerm = "tennis";
+    String websiteAddress = "https://www.milton.edu/";
 
     public HTMLRead() {
+        mainFrame = new JFrame();
+        searchBox = new JTextField("milton");
+        goButton = new JButton("Go");
+        newPanel = new JPanel();
+        searchResults = new JTextArea("Results will populate here");
+        website = new JTextField("https://www.milton.edu/athletics/");
+        goButton.addActionListener(this);
+        scrollPane = new JScrollPane(searchResults);
+
+        searchResults.setEditable(false);
+        mainFrame.setLayout(new BorderLayout(10, 10));
+        newPanel.setLayout(new FlowLayout());
+
+        mainFrame.add(newPanel, BorderLayout.NORTH);
+        mainFrame.add(scrollPane, BorderLayout.CENTER);
+        newPanel.add(website);
+        newPanel.add(searchBox);
+        newPanel.add(goButton);
+
+
+        mainFrame.pack();
+        mainFrame.setSize(1600, 800);
+        website.setSize(100, 100);
+        searchBox.setSize(100, 100);
+        mainFrame.setVisible(true);
+
+        populateResults();
+
+    }
+    public void populateResults() {
         try {
-            URL url = new URL("https://www.milton.edu/athletics/");
+            System.out.println("populateResults ran");
+            URL url = new URL(websiteAddress);
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(url.openStream())
             );
             String line;
-            String searchTerm = "tennis";
             while ((line = reader.readLine()) != null) {
-                if (line.contains("href")) {
+                if (line.contains("a href")) {
                     if (line.contains(searchTerm)) {
-                        int index = line.indexOf("href");
-                        String differentString = line.substring((index + 6));
-                        int nextQuote = differentString.indexOf(">");
-                        String link = differentString.substring(0, (nextQuote-1));
-                        System.out.println("index " + index + ": " + link);
+                        int index = line.indexOf("a href");
+                        String differentString = line.substring((index + 8));
+                        System.out.println(differentString);
+                        int nextQuote = differentString.indexOf("\"");
+                        String link = differentString.substring(0, (nextQuote));
+                        searchResults.setText((searchResults.getText() + "\n" + index + ": " + link));
+                        System.out.println(index + ": " + link);
                     }
                 }
             }
 
             reader.close();
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object buttonClicked = e.getSource();
+        if (buttonClicked == goButton) {
+            searchTerm = searchBox.getText();
+            websiteAddress = website.getText();
+            System.out.println("buttonclicked");
+            searchResults.setText("");
+            populateResults();
+        }
+
     }
 
 }
